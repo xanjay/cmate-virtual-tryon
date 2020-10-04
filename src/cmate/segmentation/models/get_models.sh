5bd0fe72a860b0f5ff7b0ffc33a0bf041c3a7207
sanjay <sanjayparajuli27@gmail.com>
2020-10-04 22:26:27 +0545

    Added model download script.

---
+++ src/cmate/segmentation/models/get_models.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Downloading model files..."
+
+# deeplab model
+DEEPLAB_URL="https://www.dropbox.com/s/1a2hyzuiol6g1w1/frozen_inference_graph-110871.pb?dl=1"
+DEEPLAB_FOLDER="deeplab"
+DEEPLAB_MODEL="frozen_inference_graph-110871.pb"
+
+# opepose body_25 model
+BODY_25_URL="https://www.dropbox.com/sh/pfrrd0znex1nerz/AABVEnY0ptgrc9iGmjQjBjRDa/pose_iter_584000.caffemodel?dl=1"
+BODY_25_FOLDER="mpii_openpose_body25"
+BODY_25_MODEL="pose_iter_584000.caffemodel"
+
+wget -c ${DEEPLAB_URL} -O ${DEEPLAB_FOLDER}/${DEEPLAB_MODEL}
+wget -c ${BODY_25_URL} -O ${BODY_25_FOLDER}/${BODY_25_MODEL}
+
+echo "Model files donwloaded."
